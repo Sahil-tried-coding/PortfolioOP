d44d7481bbbc94c3fe7899c398894f25f4aad1de
Sahil-tried-coding <sahiltriedcoding@gmail.com>
2024-06-23 05:57:57 -0400

    third change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ function Contact() {
       </div>
       <div className="sm:text-2xl font-semibold g text-center">Talk to me.</div>
       <div className="flex gap-3  text-Rubik font-Inter  font-semibold text-lg text-white flex-col items-center justify-center sm:flex-row">
-        <div className="bg-purple-700 mt-3 sm:w-[300px] sm:gap-0 gap-1 flex flex-col items-center justify-center rounded-lg  w-[70%] h-[160px]">
+        <div className="bg-purple-700 mt-3 pt-3 sm:w-[300px] sm:gap-0 gap-1 flex flex-col items-center justify-center rounded-lg  w-[70%] h-[160px]">
           <MdOutlineEmail />
           <span>E-mail</span>
           <br />
@@ -26,20 +26,20 @@ function Contact() {
           <br />
           <span className="flex items-center gap-2">
             <a
-              className="flex items-center gap-2"
+              className="flex items-center "
               href="mailto:sahiltriedcoding@gmail.com"
             >
               Mail me <FaArrowRight />
             </a>
           </span>
         </div>
-        <div className="bg-purple-700 sm:w-[300px] sm:gap-0 gap-1 flex flex-col items-center justify-center rounded-lg  w-[70%] h-[160px]">
+        <div className="bg-purple-700 sm:w-[300px] pt-4  gap-1 flex flex-col items-center sm:justify-center  rounded-lg  w-[70%] h-[160px]">
           <BsWhatsapp />
           <span>Whatsapp</span>
           <br />
           <span className="font-normal text-xs">+91 7972483330</span>
           <br />
-          <span className="flex items-center gap-2">
+          <span className="flex items-center ">
             <a
               className="flex items-center gap-2"
               href="https://web.whatsapp.com/send/?phone=9359670045&text=hello"
